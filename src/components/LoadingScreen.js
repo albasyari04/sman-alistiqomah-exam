b97b848ebfd6e22ccc043460320f84df931be76a
M. Naufal Jamil <albasyarialfaruq@gmail.com>
2026-07-19 00:43:28 +0700

    perbaikan loading

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,13 +1,21 @@
-import { View, ActivityIndicator, StyleSheet } from 'react-native'
+import { View, ActivityIndicator, StyleSheet, Image } from 'react-native'
 
 export default function LoadingScreen() {
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color="#1B5E20" />
+      <Image source={require('../../assets/icon.png')} style={styles.logo} resizeMode="contain" />
+      <ActivityIndicator size="large" color="#ffffff" style={styles.indicator} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0E3D2B',
+  },
+  logo: { width: 140, height: 140, marginBottom: 18 },
+  indicator: { marginTop: 6 },
 })
